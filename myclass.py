06e36125fed728c0216f743eae08231162620e13
meruert111 <mmerkinbekovna@gmail.com>
2019-02-08 19:25:52 +0500

    initial commit

---
+++ myclass.py
@@ -0,0 +1,8 @@
+class Circle:
+	def __init__(self, raduis=10, x=0, y=0):
+		self.raduis=raduis
+		self.x=x
+		self.y=y
+	
+	def values(self):
+		print("Raduis= ",self.raduis, "x=", self.x, "y=", self.y)
